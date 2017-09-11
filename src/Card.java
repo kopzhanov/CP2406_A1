3072b90a61ccd996883478f2d3425df1076813cd
Alikhan Kopzhanov <alikhan.kopzhanov@my.jcu.edu.au>
2017-09-11 16:31:39 +0800

    Removed MineralCard Class (moved into parent Card Class)

--- src/Card.java
+++ src/Card.java
@@ -1,11 +1,51 @@
 public class Card {
     private String name;
+    private double hardness;
+    private double gravity;
+    private String cleavage;
+    private String abundance;
+    private String ecoValue;
+    private String instruction;
 
-    Card(String name) {
+    public Card(String name, double hardness, double gravity, String cleavage, String abundance, String ecoValue) {
         this.name = name;
+        this.hardness = hardness;
+        this.gravity = gravity;
+        this.cleavage = cleavage;
+        this.abundance = abundance;
+        this.ecoValue = ecoValue;
+    }
+
+    public Card(String name, String instruction) {
+        this.name = name;
+        this.instruction = instruction;
     }
 
     public String getName() {
         return name;
+    }
+
+    public double getHardness() {
+        return hardness;
+    }
+
+    public double getGravity() {
+        return gravity;
+    }
+
+    public String getCleavage() {
+        return cleavage;
+    }
+
+    public String getAbundance() {
+        return abundance;
+    }
+
+    public String getEcoValue() {
+        return ecoValue;
+    }
+
+    public String getInstruction() {
+        return instruction;
     }
 }
